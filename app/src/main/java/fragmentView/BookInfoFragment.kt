6016cd5f425fe637dedcdc9d54f44b60563a1601
hyunho058 <hyunho058@naver.com>
2020-06-02 17:47:16 +0900

    Add Fragment View

--- app/src/main/java/fragmentView/BookInfoFragment.kt
+++ app/src/main/java/fragmentView/BookInfoFragment.kt
@@ -1,4 +1,19 @@
 package fragmentView
 
-class BookInfoFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BookInfoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
