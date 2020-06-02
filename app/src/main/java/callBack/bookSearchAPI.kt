5afaf538ee23c5016f8ed7d84116376b523dbb9f
hyunho058 <lhyun058@gmail.com>
2020-06-03 01:33:31 +0900

    Update KakaoAPI request

--- app/src/main/java/callBack/bookSearchAPI.kt
+++ app/src/main/java/callBack/bookSearchAPI.kt
@@ -1,4 +1,24 @@
 package callBack
 
-class bookSearchAPI {
+import android.os.AsyncTask
+import model.Document
+
+class bookSearchAPI constructor(keyword : String) : AsyncTask<String, Void, ArrayList<Document>>() {
+
+    val mTAG : String = "bookSearchAPI"
+    val token : String = "KakaoAK a85301089026f3d76b61ac72f59b1d91"
+    var keyword : String = ""
+    var documentList : ArrayList<Document> = ArrayList()
+
+////    constructor()
+//
+//    constructor(keyword : String):this(){
+//        this.keyword = keyword
+//    }
+//
+//    constructor()
+
+    override fun doInBackground(vararg params: String?): ArrayList<Document> {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/model/Document.kt
+++ app/src/main/java/model/Document.kt
@@ -1,4 +1,16 @@
 package model
 
 class Document {
+    var authors : ArrayList<String> = ArrayList()
+    var contents : String = ""
+    var datetime:String = ""
+    var isbn:String=""
+    var price:String=""
+    var publisher:String=""
+    var sale_price:String=""
+    var status:String=""
+    var thumbnail:String=""
+    var title:String=""
+    var translators:ArrayList<String> = ArrayList()
+    var url:String=""
 }
