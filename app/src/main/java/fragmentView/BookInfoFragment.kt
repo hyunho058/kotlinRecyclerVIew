0bb59ac26b080ab27b4a3d7d7d00503a268c4022
hyunho058 <lhyun058@gmail.com>
2020-06-12 00:51:43 +0900

    update Fragment

--- app/src/main/java/fragmentView/BookInfoFragment.kt
+++ app/src/main/java/fragmentView/BookInfoFragment.kt
@@ -1,19 +1,29 @@
 package fragmentView
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class BookInfoFragment : Fragment() {
+    val TAG : String = "BookInfoFragment"
+    lateinit var tvTitle : TextView
+    lateinit var tvAuthor : TextView
+    lateinit var tvPrice : TextView
+    lateinit var  tvISBN : TextView
+    lateinit var ivBookImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
+        Log.v(TAG,"onCreateView");
+        tvTitle =
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
