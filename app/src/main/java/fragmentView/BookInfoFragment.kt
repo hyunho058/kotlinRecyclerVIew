59509e899dd705ae40be25fa84742783adc1aae6
hyunho058 <hyunho058@naver.com>
2020-06-12 18:01:56 +0900

    Update BookinfoFragment

--- app/src/main/java/fragmentView/BookInfoFragment.kt
+++ app/src/main/java/fragmentView/BookInfoFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.kotlinrecyclerview.R
 
 class BookInfoFragment : Fragment() {
     val TAG : String = "BookInfoFragment"
@@ -17,13 +18,24 @@ class BookInfoFragment : Fragment() {
     lateinit var  tvISBN : TextView
     lateinit var ivBookImage : ImageView
 
+//    inflater.inflate(R.layout.bookinfo_fragment, container, false);
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.v(TAG,"onCreateView");
-        tvTitle =
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_book_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvTitle = view.findViewById(R.id.tvTitle)
+        tvAuthor = view.findViewById(R.id.tvAuthor)
+        tvPrice = view.findViewById(R.id.tvPrice)
+        tvISBN = view.findViewById(R.id.tvISBN)
+        ivBookImage = view.findViewById(R.id.ivBookImage)
     }
 }
