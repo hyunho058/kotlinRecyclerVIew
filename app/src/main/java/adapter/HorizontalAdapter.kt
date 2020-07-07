9120b2c855640533d3f87d2534ecc668dc82aa94
hyunho058 <hyunho058@naver.com>
2020-07-08 01:12:59 +0900

    Update Kotlin Project RecyclerView Adapter

--- app/src/main/java/adapter/HorizontalAdapter.kt
+++ app/src/main/java/adapter/HorizontalAdapter.kt
@@ -1,14 +1,25 @@
 package adapter
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import fragmentView.BookInfoFragment
 import model.Document
 
 class HorizontalAdapter : RecyclerView.Adapter<HorizontalAdapter.CardItem>(){
 
     var TAG : String = "HorizontalAdapter"
     lateinit var documentList : ArrayList<Document>
+    lateinit var context : Context
+    lateinit var bookInfoFragment : Fragment
+
+    constructor(context: Context, documentList: ArrayList<Document>, bookInfoFragment: Fragment){
+        this.context = context
+        this.documentList = documentList
+        this.bookInfoFragment = bookInfoFragment
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardItem {
         TODO("Not yet implemented")
