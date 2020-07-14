23e93b4c6b12b56c293f62ff678f39a58978d7d3
hyunho058 <hyunho058@naver.com>
2020-07-15 00:38:53 +0900

    Update HorizontalAdapter

--- app/src/main/java/adapter/HorizontalAdapter.kt
+++ app/src/main/java/adapter/HorizontalAdapter.kt
@@ -1,8 +1,11 @@
 package adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import fragmentView.BookInfoFragment
@@ -23,14 +26,17 @@ class HorizontalAdapter : RecyclerView.Adapter<HorizontalAdapter.CardItem>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardItem {
         TODO("Not yet implemented")
+        Log.v(TAG,"onCreateViewHolder()")
     }
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
+        Log.v(TAG,"getItemCount()")
     }
 
     override fun onBindViewHolder(holder: CardItem, position: Int) {
         TODO("Not yet implemented")
+        Log.v(TAG,"onBindViewHolder()")
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -38,6 +44,13 @@ class HorizontalAdapter : RecyclerView.Adapter<HorizontalAdapter.CardItem>(){
     }
 
     class CardItem(itemView: View) : RecyclerView.ViewHolder(itemView){
+        lateinit var iv_poster : ImageView
+        lateinit var tv_title : TextView
 
+        constructor(itemView: View, iv_poster ImageView, tv_title TextView) : this() {
+            super(itemView)
+            this.iv_poster = iv_poster
+            this.tv_title = tv_title
+        }
     }
 }
